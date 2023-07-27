d6a532a170e5bbd864864a562c73ee6d2d8bd418
navya1808 <63902681+navya1808@users.noreply.github.com>
2023-07-28 01:41:42 +0530

    Update jwtverify.js

--- backend/jwtverify.js
+++ backend/jwtverify.js
@@ -6,13 +6,13 @@ module.exports = (req, res, next) => {
     const token = req.headers["authorization"].split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(500).send({ message: "Auth failed ....", status: false });
+        res.status(500).send({ message: "Auth failed", status: false });
       } else {
         req.userId = decode.id;
         next();
       }
     });
   } catch (error) {
-    res.status(500).send({ message: "Auth failed hello", status: false });
+    res.status(500).send({ message: "Auth failed", status: false });
   }
 };
